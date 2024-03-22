f5fe5105f4098923b2d92584dca8dc661d05fea1
chessboy71 <bentegrimohamed@gmail.com>
2024-03-22 09:57:47 -0700

    finishing puppet up

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,10 @@
-#installing flask 
-#
+#installing flask and werkzeug
+
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
 package {'Werkzeug':
   ensure   => '2.1.1',
-  provider => 'pip3' 
+  provider => 'pip3',
 }
