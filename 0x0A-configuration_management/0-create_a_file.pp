89c6e0e440af27f29ba03847a61fa88868f59c6b
chessboy71 <bentegrimohamed@gmail.com>
2024-03-22 09:07:17 -0700

    first

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
-file {'/tmp/school':
-ensure => 'present',
-owner => 'www-data',
-group => 'www-data',
-mode => '0744',
-content => 'I love Puppet',
+file { '/tmp/school':
+ ensure => 'present',
+ owner   => 'www-data',
+ group   => 'www-data',
+ mode    => '0744',
+ content => 'I love Puppet',
 }
+
