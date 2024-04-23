383c3423e657bcd8fe38ae5e7054f5a70dd48bc1
chessboy71 <bentegrimohamed@gmail.com>
2024-04-22 17:22:25 -0700

    s

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,18 +1,8 @@
 #!/usr/bin/python3
 
 """
-Write a Python script that, using this REST API, for a given employee ID, returns information about his/her TODO list progress.
-
-Requirements:
-
-You must use urllib or requests module
-The script must accept an integer as a parameter, which is the employee ID
-The script must display on the standard output the employee TODO list progress in this exact format:
-First line: Employee EMPLOYEE_NAME is done with tasks(NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):
-EMPLOYEE_NAME: name of the employee
-NUMBER_OF_DONE_TASKS: number of completed tasks
-TOTAL_NUMBER_OF_TASKS: total number of tasks, which is the sum of completed and non-completed tasks
-Second and N next lines display the title of completed tasks: TASK_TITLE (with 1 tabulation and 1 space before the TASK_TITLE)
+Write a Python script that, using this REST API, for a given
+TASK_TITLE (with 1 tabulation and 1 space before the TASK_TITLE)
 
 """
 import sys
@@ -20,19 +10,21 @@ import requests
 
 
 if __name__ == "__main__":
+    """"""
     url = "https://jsonplaceholder.typicode.com/"
 
 userId = sys.argv[1]
 res = requests.get(url + "users/{}".format(userId)).json()
-todos_res = requests.get(url + "todos", params = {'userId': "{}".format(userId),}).json()
+todos_res = requests.get(url + "todos", params={'userId': "{}".format(userId)})
+todos_res = todos_res.json()
 finiTodo = []
 for todo in todos_res:
     if todo.get("completed") is True:
         finiTodo.append(todo.get("title"))
 
 print(
-        "Employee {} is done with tasks({}/{}):".format(res.get("name"), len(finiTodo), len(todos_res)))
+        "Employee {} is done with tasks({}/{}):"
+        .format(res.get("name"), len(finiTodo), len(todos_res)))
 
 for todo in finiTodo:
     print("\t {}".format(todo))
-    
